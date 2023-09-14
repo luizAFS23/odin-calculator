074a98fcf6859f42c7db40c3964ae39c02d79567
luizAFS23 <luizafs.1500@gmail.com>
2023-09-14 17:58:36 -0300

    changes

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const display = document.querySelector('.display').value;
 
-function add(){
-    if(display == null){
-        
-    }
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 - num2;
+}
+
+function divide(num1, num2){
+    return num1 - num2;
 }
